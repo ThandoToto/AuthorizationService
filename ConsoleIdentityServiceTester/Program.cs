aec693e59a0e9ec6fc06879bc37551de3fbbb2b5
Thando Toto <thandot@gmail.com>
2016-06-19 12:09:58 +0200

    Implementing email service

---
+++ ConsoleIdentityServiceTester/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleIdentityService.Config;
+using System.Configuration;
+using Microsoft.AspNet.Identity;
+
+namespace ConsoleIdentityServiceTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = ConfigurationManager.AppSettings["emailserver"];
+            string user = ConfigurationManager.AppSettings["emailuser"];
+            string password = ConfigurationManager.AppSettings["emailpassword"];
+            string fromAddress = ConfigurationManager.AppSettings["emailfrom"];
+            var email = new EmailService(server, user, password, fromAddress);
+            email.SendAsync(new IdentityMessage { Destination = "thando@appziko.com", Body = $"Messge sent at {DateTime.Now.Minute}", Subject="Account created" });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleIdentityService/Config/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIdentityService.Config
+{
+    public class EmailService : IIdentityMessageService
+    {
+        string _fromAddress, _emailServer, _emailUser, _emailPassword = string.Empty;
+
+        public EmailService(string emailServer, string emailUser, string emailPassword, string emailFrom)
+        {
+            _fromAddress = emailFrom;
+            _emailServer = emailServer;
+            _emailUser = emailUser;
+            _emailPassword = emailPassword;
+        }
+
+        public async Task SendAsync(IdentityMessage message)
+        {
+            MailMessage mailMessage = new MailMessage(_fromAddress, message.Destination, message.Subject, message.Body);
+            mailMessage.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient(_emailServer);
+            client.Credentials = new NetworkCredential(_emailUser, _emailPassword);
+            client.Send(mailMessage);
+
+            await Task.FromResult<int>(0);
+        }
+        
+    }
+}
+
