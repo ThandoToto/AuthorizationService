1d60795624e97da00d57df5d0ff551115af12f00
Thando Toto <thandot@gmail.com>
2016-06-20 04:27:50 +0200

    Removing redundant projects

--- SimpleIdentityServiceApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SimpleIdentityServiceApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SimpleIdentityServiceApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SimpleIdentityServiceApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
